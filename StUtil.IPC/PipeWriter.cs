0df383d039c4632fab87096e492ddeaf9c56cf2a
TGOSeraph <simon.snoopy+git@gmail.com>
2013-12-01 04:38:01 -0800

    Added IPC Library

---
+++ StUtil.IPC/PipeWriter.cs
@@ -0,0 +1,41 @@
+﻿using StUtil.Misc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StUtil.IPC
+{
+    public class PipeWriter : DisposableBase
+    {
+        public PipeWriter()
+        {
+                //var server = new NamedPipeServerStream("PipesOfPiece");
+                //server.WaitForConnection();
+                //StreamReader reader = new StreamReader(server);
+                //StreamWriter writer = new StreamWriter(server);
+                //while (true)
+                //{
+                //    var line = reader.ReadLine();
+                //    writer.WriteLine(String.Join("", line.Reverse()));
+                //    writer.Flush();
+                //}
+            String[] listOfPipes = System.IO.Directory.GetFiles(@"\\.\pipe\");
+
+            try
+            {
+                var client = new NamedPipeClientStream("PipesOfPiece");
+                client.Connect();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+          
+        }
+    }
+}
