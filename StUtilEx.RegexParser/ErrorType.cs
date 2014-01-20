0e378d4cb0097b89f770d87cbdecf36dd0d7a8a3
TGOSeraph <simon.snoopy+git@gmail.com>
2014-01-20 07:21:15 -0800

    Added Regexparser

---
+++ StUtilEx.RegexParser/ErrorType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StUtilEx.RegexParser
+{
+    public enum ErrorType
+    {
+        None,
+        InvalidSyntax,
+        MissingClosingPairing,
+        MissingOpeningPairing,
+        ExpectedNumber
+    }
+}
