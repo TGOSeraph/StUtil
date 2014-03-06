7274cbc8223e1608a6eed2a00fc0584b93911e8a
TGOSeraph <seraph.it@gmail.com>
2014-03-06 12:39:57 +0000

    Updates

---
+++ StUtil.Core/Utilities/DisposableBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StUtil.Misc
+{
+    public abstract class DisposableBase : IDisposable
+    {
+        public bool Disposed { get; private set; }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    CleanUpNativeResources();
+                }
+                Disposed = true;
+            }
+            CleanUpManagedResources();
+        }
+
+        protected virtual void CleanUpManagedResources()
+        {
+        }
+        protected virtual void CleanUpNativeResources()
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- StUtil.Tests/Program.cs
+++ StUtil.Tests/Program.cs
@@ -7,81 +7,269 @@ using StUtil.Extensions;
 
 namespace StUtil.Tests
 {
-    using StUtil.CodeGen.CodeObjects.Misc;
-    using StUtil.CodeGen.CodeObjects.CodeStructures;
-    using StUtil.CodeGen.CodeObjects.Generic;
-    using StUtil.CodeGen.CodeObjects.Data;
-    using StUtil.CodeGen.CodeObjects.Attributes;
-    using StUtil.CodeGen.CodeObjects.Syntax;
-    using System.Runtime.InteropServices;
-
     static class Program
     {
-        private const uint WM_DROPFILES = 0x233;
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        public enum TokenType
+        {
+            None,
+            Function,
+            Parameters,
+            Parameter,
+            ParameterSplitter,
+            PopupTimeout,
+            SetVariableString,
+            SetVariableFunction,
+            SetVariableValue
+        }
+        public class Token
+        {
+            public TokenType Type { get; set; }
+            public string Value { get; set; }
+            public Token(string value, TokenType type)
+            {
+                this.Type = type;
+                this.Value = value;
+            }
+            public override string ToString()
+            {
+                return "[" + Type.ToString() + "] " + Value;
+            }
+        }
+
+        static Dictionary<string, string> GetScriptArguments()
+        {
+            return new Dictionary<string, string>() { 
+                {"objecttype", "contact"},
+                {"objectid", "1234"}
+            };
+        }
+
+        static Dictionary<string, Func<string>> GetReplacements()
+        {
+            return new Dictionary<string, Func<string>>() { 
+                {"#", () => 
+                    {
+                        //lookup window name
+                        return "w_...";
+                    }
+                },
+                {"%", () => 
+                    {
+                        //lookup dw name
+                        return "d_...";
+                    }
+                },
+                {"@", () => 
+                    {
+                        //lookup window name
+                        return "active window handle";
+                    }
+                },
+                {"0", () => 
+                    {
+                        //blank for blank
+                        return "";
+                    }
+                }
+            };
+        }
+
+        static List<Token> FirstParse(string line)
+        {
+            string current = "";
+            TokenType type = TokenType.None;
+            List<Token> tokens = new List<Token>();
+            int bracketDepth = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+                switch (c)
+                {
+                    case '(':
+                        bracketDepth++;
+                        if (type == TokenType.None)
+                        {
+                            type = TokenType.Function;
+                            tokens.Add(new Token(current, type));
+                            type = TokenType.Parameters;
+                            current = "";
+                        }
+                        else
+                        {
+                            current += c;
+                        }
+                        break;
+                    case ')':
+                        if (--bracketDepth == 0)
+                        {
+                            tokens.Add(new Token(current, type));
+                            current = "";
+                            type = TokenType.None;
+                        }
+                        else
+                        {
+                            current += c;
+                        }
+                        break;
+                    case '~':
+                        if (current != "")
+                        {
+                            if (type == TokenType.None)
+                            {
+                                type = TokenType.Function;
+                            }
+                            tokens.Add(new Token(current, type));
+                        }
+                        type = TokenType.None;
+                        current = "";
+                        break;
+                    default:
+                        if (type == TokenType.None)
+                        {
+                            if (c == ':' && line[i + 1] == '=')
+                            {
+                                type = TokenType.SetVariableString;
+                                tokens.Add(new Token(current, type));
+                                current = "";
+                                type = TokenType.SetVariableValue;
+                                i++;
+                                break;
+                            }
+                            else if (c == '@' && line[i + 1] == '=')
+                            {
+                                type = TokenType.SetVariableFunction;
+                                tokens.Add(new Token(current, type));
+                                current = "";
+                                type = TokenType.SetVariableValue;
+                                i++;
+                                break;
+                            }
+                            else if (c == '[' && i == 0)
+                            {
+                                type = TokenType.ParameterSplitter;
+                                tokens.Add(new Token(line[++i].ToString(), type));
+                                current = "";
+                                type = TokenType.None;
+                                i++;
+                                break;
+                            }
+                        }
+                        current += c;
+                        break;
+                }
+            }
+            if (current.Length > 0)
+            {
+                if (type == TokenType.None)
+                {
+                    throw new Exception();
+                }
+                tokens.Add(new Token(current, type));
+            }
+            return tokens;
+        }
+
+        static bool ParseParameter(ref int vtmpId, string arg, Dictionary<string, string> scriptArgs, List<Token> tokens, int index)
+        {
+            bool changed = false;
+
+            if (arg.StartsWith("[") && arg.EndsWith("]"))
+            {
+                changed = true;
+                tokens.Insert(index, new Token(scriptArgs[arg.Substring(1, arg.Length - 2)], TokenType.Parameter));
+            }
+            else if (arg.StartsWith("{") && arg.EndsWith("}"))
+            {
+                string action = arg.Substring(1, arg.Length - 2);
+                if (!action.StartsWith("$"))
+                {
+                    changed = true;
+                    tokens.RemoveAt(index);
+                    ParseFunctionParameter(ref vtmpId, action, tokens, index);
+                }
+            }
+
+            return changed;
+        }
+
+        static string GetNextVariableName(ref int vtmpId)
+        {
+            return "__tmp_" + (++vtmpId).ToString();
+        }
+        static string GetVariableName(int vtmpId)
+        {
+            return "__tmp_" + vtmpId.ToString();
+        }
+
+        static void ParseFunctionParameter(ref int vtmpId, string action, List<Token> tokens, int index)
+        {
+            tokens.Insert(index - 1, new Token(GetNextVariableName(ref vtmpId), TokenType.SetVariableFunction));
+            tokens.Insert(index, new Token(action, TokenType.SetVariableValue));
+            tokens.Insert(index + 2, new Token("{$" + GetVariableName(vtmpId) + "}", TokenType.Parameter));
+        }
+
+        static bool ReParse(ref int vtmpId, Dictionary<string, string> scriptArgs, List<Token> tokens)
+        {
+            bool changed = false;
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                Token token = tokens[i];
+                switch (token.Type)
+                {
+                    case TokenType.SetVariableFunction:
+                        changed = true;
+                        token.Type = TokenType.SetVariableString;
+                        token = tokens[++i];
+                        string action = token.Value;
+                        token.Value = "{^}";
+                        foreach (Token t in FirstParse(action))
+                        {
+                            tokens.Insert(i++ - 1, t);
+                        }
+                        break;
+                    case TokenType.Parameter:
+                        if (ParseParameter(ref vtmpId, token.Value, scriptArgs, tokens, i))
+                        {
+                            changed = true;
+                        }
+                        break;
+                    case TokenType.Parameters:
+                        bool split = i - 2 >= 0 && tokens.ElementAt(i - 2).Type == TokenType.ParameterSplitter;
+                        char paramsplit = split ? tokens.ElementAt(i - 2).Value[0]
+                            : ',';
+                        string[] args = token.Value.Split(paramsplit);
+                        if (split)
+                        {
+                            tokens.RemoveAt(i - 2);
+                        }
+                        changed = true;
+                        tokens.Replace(i - 1, args.Select(a => new Token(a, TokenType.Parameter)).ToArray());
+                        break;
+                }
+            }
+            return changed;
+        }
+
         [STAThread]
         static void Main()
         {
+            Dictionary<string, string> ScriptArguments = GetScriptArguments();
+            Dictionary<string, Func<string>> Replacements = GetReplacements();
+
+            string line = "[']A({B()})";
+            List<Token> tokens = FirstParse(line);
+
+            int vtmpId = 0;
+
+            bool changed = true;
+            while (changed)
+            {
+                changed = ReParse(ref vtmpId, ScriptArguments, tokens);
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-
-
-
-
-
-        //        procedure DoDropFiles(Wnd: HWND; Files: TStringList);
-
-        //var
-        //  Size: Cardinal;
-        //  DropFiles: PDropFiles;
-        //  Run: PChar;
-        //  MemHandle: THandle;
-        //  I: Integer;
-
-        //begin
-        //  // first determine size of string buffer we have to allocate
-        //  Size := 0;
-        //  for I := 0 to Files.Count - 1 do
-        //  begin
-        //    // number of characters per string (as ANSI) plus one #0 terminator
-        //    Inc(Size, Length(Files[I]) + 1);
-        //  end;
-        //  if Size > 0 then
-        //  begin
-        //    // entire string list is terminated by another #0, add drop files structure size too
-        //    Inc(Size, 1 + SizeOf(TDropFiles));
-        //    // allocate globally accessible memory
-        //    MemHandle := GlobalAlloc(GHND or GMEM_SHARE, Size);
-        //    DropFiles := GlobalLock(MemHandle);
-        //    // fill the header
-        //    with DropFiles^ do
-        //    begin
-        //      pFiles := SizeOf(TDropFiles); // offset of file list, it follows immediately the structure
-        //      pt := Point(0, 0);            // drop point (client coords), not important here
-        //      fNC := False;                 // is it on NonClient area }, not important here
-        //      fWide := False;               // WIDE character switch, we pass ANSI string in this routine
-        //    end;
-        //    // and finally the file names
-        //    Run := Pointer(DropFiles);
-        //    Inc(Run, SizeOf(TDropFiles));
-        //    for I := 0 to Files.Count - 1 do
-        //    begin
-        //      StrPCopy(Run, Files[I]);
-        //      Inc(Run, Length(Files[I]));
-        //    end;
-        //    // put a final #0 character at the end
-        //    Run^ := #0;
-        //    // release the lock we have to the memory,...
-        //    GlobalUnlock(MemHandle);
-        //    // ...do the message...
-        //    SendMessage(Wnd, WM_DROPFILES, MemHandle, 0);
-        //    // ... and finally release the memory
-        //    GlobalFree(MemHandle);
-        //  end;
-        //end;
     }
 }
