b314eb7cee22c9acce96ea2998d27d3bd53a5699
TGOSeraph <seraph.it@gmail.com>
2014-04-10 15:59:07 +0100

    Added TriState TreeView classes

---
+++ StUtil.UI/Controls/TriStateTreeNode.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StUtil.UI.Controls
+{
+    /// NodeLineType enumeration
+    /// Type of dotted line to draw when drawing a node.
+    /// </summary>
+    internal enum NodeLineType
+    {
+        None,
+        Straight,
+        WithChildren
+    }
+
+    /// <summary>
+    ///  TriStateTreeNode class
+    /// </summary>
+    public class TriStateTreeNode : TreeNode
+    {
+        // stores node state ( either checked, unchecked or indeterminate )
+        private CheckState _nodeCheckState = CheckState.Unchecked;
+
+        // determines if the node should be regarded a container. 
+        // Only a folder can have an third (indeterminate) state
+        private bool _isContainer = true;
+        private bool _checkboxVisible = true;
+
+        // public constructor
+        public TriStateTreeNode() : base() { }
+        public TriStateTreeNode(string text) : base(text) { }
+        public TriStateTreeNode(string text, int imageIndex, int selectedImageIndex) : base(text, imageIndex, selectedImageIndex) { }
+        public TriStateTreeNode(string text, int imageIndex, int selectedImageIndex, TriStateTreeNode[] children) : base(text, imageIndex, selectedImageIndex, children) { }
+        public TriStateTreeNode(string text, TriStateTreeNode[] children) : base(text, children) { }
+
+
+        /// <summary>
+        /// Get / set if the node is checked or not.
+        /// </summary>
+        [Browsable(false)]
+        new public bool Checked
+        {
+            get { return (this._nodeCheckState != CheckState.Unchecked); }
+            set { SetCheckedState(value ? CheckState.Checked : CheckState.Unchecked); }
+        }
+
+        /// <summary>
+        /// Get's the node's current state, either checked / unchecked for non-container nodes,
+        /// or checked / unchecked / indeterminate for container nodes.
+        /// </summary>
+        public CheckState CheckState
+        {
+            get { return this._nodeCheckState; }
+        }
+
+        /// <summary>
+        /// Determines if the node should act as a container
+        /// </summary>
+        public bool IsContainer
+        {
+            get { return _isContainer; }
+            set { _isContainer = value; }
+        }
+
+        public bool CheckboxVisible
+        {
+            get { return this._checkboxVisible; }
+            set { this._checkboxVisible = value; }
+        }
+
+        internal NodeLineType NodeLineType
+        {
+            get
+            {
+                // is this node bound to a treeview ?
+                if (null != this.TreeView)
+                {
+                    // do we need to draw lines at all?
+                    if (!this.TreeView.ShowLines) { return NodeLineType.None; }
+                    if (this.CheckboxVisible) { return NodeLineType.None; }
+
+                    if (this.Nodes.Count > 0)
+                    {
+                        return NodeLineType.WithChildren;
+                    }
+                    return NodeLineType.Straight;
+                }
+
+                // no treeview so this node will never been drawn at all
+                return NodeLineType.None;
+            }
+        }
+
+        /// <summary>
+        /// Used internally by the treeview to set a node to s specific state.
+        /// </summary>
+        /// <param name="value"></param>
+        internal void SetCheckedState(CheckState value)
+        {
+            CheckStateChanged(_nodeCheckState, value);
+        }
+
+        /// <summary>
+        /// Private method that is called by one of the childnodes in order to report a
+        /// change in state.
+        /// </summary>
+        /// <param name="childNewState">New state of the childnode in question</param>
+        private void ChildCheckStateChanged(CheckState childNewState)
+        {
+            bool notifyParent = false;
+            CheckState currentState = this._nodeCheckState;
+            CheckState newState = this._nodeCheckState;
+
+            // take action based on the child's new state
+            switch (childNewState)
+            {
+                case CheckState.Indeterminate: // child state changed to indeterminate
+                    // if one of the children's state changes to indeterminate, 
+                    // it's parent should do the same, if it is a container too!.
+                    if (IsContainer)
+                    {
+                        newState = CheckState.Indeterminate;
+
+                        // the same is valid for this node's parent.
+                        // check if this node's state has changed and inform the parent
+                        // if this is the case
+                        notifyParent = (newState != currentState);
+                    }
+                    break;
+                case CheckState.Checked:
+                    // One of the child nodes was checked so we must check:
+                    // 1) if the child node is the only child node, our state becomes checked too.
+                    // 2) if there are children with a state other then checked, our state
+                    //    must become indeterminate if this is a container.
+                    if (this.Nodes.Count == 1) // if there is only one child, our state changes too!
+                    {
+                        // set our state to checked too and set the flag for
+                        // parent notification. 
+                        newState = CheckState.Checked;
+                        notifyParent = true;
+                        break;
+                    }
+
+                    // set to checked by default
+                    // if this is not a container, there is no need to check further
+                    newState = CheckState.Checked;
+                    if (!IsContainer)
+                    {
+                        notifyParent = (newState != currentState);
+                        break;
+                    }
+
+                    // traverse all child nodes to see if there are any with a state other then
+                    // checked. if so, change state to indeterminate.
+                    foreach (TreeNode node in this.Nodes)
+                    {
+                        TriStateTreeNode checkedNode = node as TriStateTreeNode;
+                        if (checkedNode != null && checkedNode.CheckState != CheckState.Checked)
+                        {
+                            newState = CheckState.Indeterminate;
+                            break;
+                        }
+                    }
+
+                    // set notification flag if our state has to be changed too
+                    notifyParent = (newState != currentState);
+                    break;
+                case CheckState.Unchecked:
+                    // For nodes that are no containers, a child being unchecked is not relevant.
+                    // so we can exit at this point.
+                    if (!IsContainer)
+                        break;
+
+                    // A child's state has changed to unchecked so check:
+                    // 1) if this is the only child. if so, uncheck this node too, if it is a container, and set
+                    //    notification flag for the parent.
+                    // 2) Check if there are child nodes with a state other then unchecked.
+                    //    if so, change our state to indeterminate.
+                    if (this.Nodes.Count == 1)
+                    {
+                        // synchronize state with only child.
+                        // set notification flag
+                        newState = CheckState.Unchecked;
+                        notifyParent = true;
+                        break;
+                    }
+
+                    // set to unchecked by default
+                    newState = CheckState.Unchecked;
+
+                    // if there is a child with a state other then unchecked,
+                    // our state must become indeterminate.
+                    foreach (TreeNode node in this.Nodes)
+                    {
+                        TriStateTreeNode checkedNode = node as TriStateTreeNode;
+                        if (checkedNode != null && checkedNode.CheckState != CheckState.Unchecked)
+                        {
+                            newState = CheckState.Indeterminate;
+                            break;
+                        }
+                    }
+
+                    // notify the parent only if our state is about to be changed too.
+                    notifyParent = (newState != currentState);
+                    break;
+            }
+
+            // should we notify the parent? ( has our state changed? )
+            if (notifyParent)
+            {
+                // change state
+                this._nodeCheckState = newState;
+
+                // notify parent
+                if (this.Parent != null)
+                {
+                    TriStateTreeNode parentNode = this.Parent as TriStateTreeNode;
+                    if (parentNode != null)
+                    {
+                        // call the same method on the parent.
+                        parentNode.ChildCheckStateChanged(this._nodeCheckState);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is only called when a node's state has been changed through the 
+        /// Checked property
+        /// </summary>
+        /// <param name="oldState">Previous state</param>
+        /// <param name="newState">New state</param>
+        private void CheckStateChanged(CheckState oldState, CheckState newState)
+        {
+            // States are equal?
+            if (newState != oldState)
+            {
+                // not equal.
+                // modify state
+                this._nodeCheckState = newState;
+
+                // change state of the children
+                if (this.Nodes != null && this.Nodes.Count > 0)
+                {
+                    foreach (TreeNode node in this.Nodes)
+                    {
+                        TriStateTreeNode tsNode = node as TriStateTreeNode;
+                        if (tsNode != null)
+                        {
+                            tsNode.ChangeChildState(newState);
+                        }
+                    }
+                }
+
+                // notify the parent of the changed state.
+                if (this.Parent != null)
+                {
+                    TriStateTreeNode parentNode = this.Parent as TriStateTreeNode;
+                    if (parentNode != null)
+                    {
+                        parentNode.ChildCheckStateChanged(this._nodeCheckState);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is only called by other nodes so it can be private.
+        /// Changes state of the node to the state provided.
+        /// </summary>
+        /// <param name="newState"></param>
+        private void ChangeChildState(CheckState newState)
+        {
+            // change state
+            this._nodeCheckState = newState;
+
+            // change state on the children
+            if (this.Nodes != null && this.Nodes.Count > 0)
+            {
+                foreach (TreeNode node in this.Nodes)
+                {
+                    TriStateTreeNode tsNode = node as TriStateTreeNode;
+                    if (tsNode != null)
+                    {
+                        tsNode.ChangeChildState(newState);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StUtil.UI/Controls/TriStateTreeView.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace StUtil.UI.Controls
+{
+    public class TriStateTreeView : TreeView
+    {
+        private int indexUnchecked;
+        private int indexChecked;
+        private int indexIndeterminate;
+        private bool useCustomImages;
+
+        public TriStateTreeView() : base() { }
+
+        [Category("CheckState")]
+        [DefaultValue(false)]
+        public bool UseCustomImages
+        {
+            get { return this.useCustomImages; }
+            set { this.useCustomImages = value; }
+        }
+
+        [Category("CheckState")]
+        [TypeConverter(typeof(TreeViewImageIndexConverter))]
+        [Editor("System.Windows.Forms.Design.ImageIndexEditor, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+        [DefaultValue(0)]
+        public int CheckedImageIndex
+        {
+            get
+            {
+                if (base.ImageList == null)
+                    return -1;
+                if (this.indexChecked >= this.ImageList.Images.Count)
+                    return Math.Max(0, this.ImageList.Images.Count - 1);
+                return this.indexChecked;
+            }
+            set
+            {
+                if (value == -1)
+                    value = 0;
+                if (value < 0)
+                    throw new ArgumentException(string.Format("Index out of bounds! ({0}) index must be equal to or greater then {1}.", value.ToString(), "0"));
+                if (this.indexChecked != value)
+                {
+                    this.indexChecked = value;
+                    if (base.IsHandleCreated)
+                        base.RecreateHandle();
+                }
+            }
+        }
+
+        [Category("CheckState")]
+        [TypeConverter(typeof(TreeViewImageIndexConverter))]
+        [Editor("System.Windows.Forms.Design.ImageIndexEditor, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+        [DefaultValue(0)]
+        public int UncheckedImageIndex
+        {
+            get
+            {
+                if (base.ImageList == null)
+                    return -1;
+                if (this.indexUnchecked >= this.ImageList.Images.Count)
+                    return Math.Max(0, this.ImageList.Images.Count - 1);
+                return this.indexUnchecked;
+            }
+            set
+            {
+                if (value == -1)
+                    value = 0;
+                if (value < 0)
+                    throw new ArgumentException(string.Format("Index out of bounds! ({0}) index must be equal to or greater then {1}.", value.ToString(), "0"));
+                if (this.indexUnchecked != value)
+                {
+                    this.indexUnchecked = value;
+                    if (base.IsHandleCreated)
+                        base.RecreateHandle();
+                }
+            }
+        }
+
+        [Category("CheckState")]
+        [TypeConverter(typeof(TreeViewImageIndexConverter))]
+        [Editor("System.Windows.Forms.Design.ImageIndexEditor, System.Design, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+        [DefaultValue(0)]
+        public int IndeterminateImageIndex
+        {
+            get
+            {
+                if (base.ImageList == null)
+                    return -1;
+                if (this.indexIndeterminate >= this.ImageList.Images.Count)
+                    return Math.Max(0, this.ImageList.Images.Count - 1);
+                return this.indexIndeterminate;
+            }
+            set
+            {
+                if (value == -1)
+                    value = 0;
+                if (value < 0)
+                    throw new ArgumentException(string.Format("Index out of bounds! ({0}) index must be equal to or greater then {1}.", value.ToString(), "0"));
+                if (this.indexIndeterminate != value)
+                {
+                    this.indexIndeterminate = value;
+                    if (base.IsHandleCreated)
+                        base.RecreateHandle();
+                }
+            }
+        }
+
+#pragma warning disable 649
+        private struct RECT
+        {
+            internal int left;
+            internal int top;
+            internal int right;
+            internal int bottom;
+        }
+
+#pragma warning disable 649
+        private struct NMHDR
+        {
+            internal IntPtr hwndFrom;
+            internal IntPtr idFrom;
+            internal int code;
+        }
+
+#pragma warning disable 649
+        private struct NMCUSTOMDRAW
+        {
+            internal NMHDR hdr;
+            internal int dwDrawStage;
+            internal IntPtr hdc;
+            internal RECT rc;
+            internal IntPtr dwItemSpec;
+            internal int uItemState;
+            internal IntPtr lItemlParam;
+        }
+
+#pragma warning disable 649
+        private struct NMTVCUSTOMDRAW
+        {
+            internal NMCUSTOMDRAW nmcd;
+            internal int clrText;
+            internal int clrTextBk;
+            internal int iLevel;
+        }
+
+        private int HandleNotify(Message msg)
+        {
+            const int NM_FIRST = 0;
+            const int NM_CUSTOMDRAW = NM_FIRST - 12;
+
+            // Drawstage
+            const int CDDS_PREPAINT = 0x1;
+            const int CDDS_POSTPAINT = 0x2;
+
+            const int CDDS_ITEM = 0x10000;
+            const int CDDS_ITEMPREPAINT = (CDDS_ITEM | CDDS_PREPAINT);
+            const int CDDS_ITEMPOSTPAINT = (CDDS_ITEM | CDDS_POSTPAINT);
+
+            // Custom draw return flags
+            const int CDRF_DODEFAULT = 0x0;
+            const int CDRF_NOTIFYPOSTPAINT = 0x10;
+            const int CDRF_NOTIFYITEMDRAW = 0x20;
+
+            NMHDR tNMHDR;
+            NMTVCUSTOMDRAW tNMTVCUSTOMDRAW;
+            int iResult = 0;
+            object obj;
+            TreeNode node;
+            TriStateTreeNode tsNode;
+
+            try
+            {
+                if (!msg.LParam.Equals(IntPtr.Zero))
+                {
+                    obj = msg.GetLParam(typeof(NMHDR));
+                    if (obj is NMHDR)
+                    {
+                        tNMHDR = (NMHDR)obj;
+                        if (tNMHDR.code == NM_CUSTOMDRAW)
+                        {
+                            obj = msg.GetLParam(typeof(NMTVCUSTOMDRAW));
+                            if (obj is NMTVCUSTOMDRAW)
+                            {
+                                tNMTVCUSTOMDRAW = (NMTVCUSTOMDRAW)obj;
+                                switch (tNMTVCUSTOMDRAW.nmcd.dwDrawStage)
+                                {
+                                    case CDDS_PREPAINT:
+                                        iResult = CDRF_NOTIFYITEMDRAW;
+                                        break;
+                                    case CDDS_ITEMPREPAINT:
+                                        iResult = CDRF_NOTIFYPOSTPAINT;
+                                        break;
+                                    case CDDS_ITEMPOSTPAINT:
+                                        node = TreeNode.FromHandle(this, tNMTVCUSTOMDRAW.nmcd.dwItemSpec);
+                                        tsNode = node as TriStateTreeNode;
+                                        if (tsNode != null)
+                                        {
+                                            Graphics graph = Graphics.FromHdc(tNMTVCUSTOMDRAW.nmcd.hdc);
+                                            PaintTreeNode(tsNode, graph);
+                                            graph.Dispose();
+                                        }
+                                        iResult = CDRF_DODEFAULT;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return iResult;
+        }
+
+        /// <summary>
+        /// Paints the node specified.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="gx"></param>
+        private void PaintTreeNode(TriStateTreeNode node, Graphics gx)
+        {
+            if (this.CheckBoxes)
+            {
+                // calculate boundaries
+                Rectangle ncRect = new Rectangle(node.Bounds.X - 35, node.Bounds.Y, 15, 15);
+
+                using (SolidBrush brush = new SolidBrush(BackColor))
+                {
+                    Rectangle r = ncRect;
+                    r.Inflate(1, 0);
+                    gx.FillRectangle(brush, r);
+                }
+                if (node.IsSelected)
+                {
+                    using (Pen pen = new Pen(SystemColors.Highlight))
+                    {
+                        gx.DrawLine(pen, new Point(ncRect.Right + 1, ncRect.Top + 1), new Point(ncRect.Right + 1, ncRect.Bottom + 1));
+                    }
+                }
+
+                // draw lines, if we are supposed to
+                if (this.ShowLines)
+                {
+                    DrawNodeLines(node, ncRect, gx);
+                }
+
+                if (node.CheckboxVisible)
+                {
+                    // now draw the checkboxes
+                    switch (node.CheckState)
+                    {
+                        case CheckState.Unchecked:      // Normal
+                            DrawCheckbox(ncRect, gx, CheckBoxState.UncheckedNormal);
+                            break;
+                        case CheckState.Checked:      // Checked
+                            DrawCheckbox(ncRect, gx, CheckBoxState.CheckedNormal);
+                            break;
+                        case CheckState.Indeterminate:    // Pushed
+                            DrawCheckbox(ncRect, gx, CheckBoxState.MixedNormal);
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws thee node lines before the checkboxes are drawn
+        /// </summary>
+        /// <param name="gx">Graphics context</param>
+        private void DrawNodeLines(TriStateTreeNode node, Rectangle bounds, Graphics gx)
+        {
+            // determine type of line to draw
+            NodeLineType lineType = node.NodeLineType;
+            if (lineType == NodeLineType.None) { return; }
+
+            using (Pen pen = new Pen(SystemColors.ControlDarkDark, 1))
+            {
+                pen.DashStyle = DashStyle.Dot;
+
+                gx.DrawLine(pen, new Point(bounds.X, bounds.Y + 10), new Point(bounds.X + 15, bounds.Y + 10));
+                if (lineType == NodeLineType.WithChildren && node.IsExpanded)
+                {
+                    gx.DrawLine(pen, new Point(bounds.X + 8, bounds.Y + 8), new Point(bounds.X + 8, bounds.Y + 16));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws a checkbox in the desired state and style
+        /// </summary>
+        /// <param name="bounds">boundaries of the checkbox</param>
+        /// <param name="gx">graphics context object</param>
+        /// <param name="buttonState">state to draw the checkbox in</param>
+        private void DrawCheckbox(Rectangle bounds, Graphics gx, CheckBoxState state)
+        {
+            // if we don't have custom images, or no imagelist, draw default images
+            if (!this.useCustomImages || (this.useCustomImages && null == this.ImageList))
+            {
+                CheckBoxRenderer.DrawCheckBox(gx, new Point(bounds.Left, bounds.Top + 3), state);
+                return;
+            }
+
+            // get the right image index
+            int imageIndex = -1;
+            if ((state & CheckBoxState.UncheckedNormal) == CheckBoxState.UncheckedNormal)
+                imageIndex = this.indexUnchecked;
+            if ((state & CheckBoxState.CheckedNormal) == CheckBoxState.CheckedNormal)
+                imageIndex = this.indexChecked;
+            if ((state & CheckBoxState.MixedNormal) == CheckBoxState.MixedNormal)
+                imageIndex = this.indexIndeterminate;
+
+            if (imageIndex > -1 && imageIndex < this.ImageList.Images.Count)
+            {
+                // index is valid so draw the image
+                this.ImageList.Draw(gx, bounds.X, bounds.Y, bounds.Width + 1, bounds.Height + 1, imageIndex);
+            }
+            else
+            {
+                // index is not valid so draw default image
+                CheckBoxRenderer.DrawCheckBox(gx, bounds.Location, state);
+            }
+        }
+
+        /// <summary>
+        /// Ovveride the WindowProcedure in order to intercept the itemdraw event
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NOTIFY = 0x4E;
+
+            int iResult = 0;
+            bool bHandled = false;
+
+            if (m.Msg == (0x2000 | WM_NOTIFY))
+            {
+                if (m.WParam.Equals(this.Handle))
+                {
+                    iResult = HandleNotify(m);
+                    m.Result = new IntPtr(iResult);
+                    bHandled = (iResult != 0);
+                }
+            }
+
+            if (!bHandled)
+                base.WndProc(ref m);
+        }
+
+        /// <summary>
+        /// override the aftercheck event in order to get the node beeing checked/unchecked
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnAfterCheck(TreeViewEventArgs e)
+        {
+            base.OnAfterCheck(e);
+
+            TreeNode node = e.Node;
+            if (node != null)
+            {
+                TriStateTreeNode clickedNode = node as TriStateTreeNode;
+                if (clickedNode.CheckboxVisible)
+                {
+                    ToggleNodeState(clickedNode);
+                }
+            }
+        }
+
+        protected override void OnAfterSelect(TreeViewEventArgs e)
+        {
+            base.OnAfterSelect(e);
+        }
+
+
+        /// <summary>
+        /// Toggles node state between checked & unchecked
+        /// </summary>
+        /// <param name="node"></param>
+        private void ToggleNodeState(TriStateTreeNode node)
+        {
+            // no need to toggle state for non-existing node ( or non-tristatetreenode! )
+            if (null == node) return;
+
+            // toggle state
+            CheckState nextState;
+            switch (node.CheckState)
+            {
+                case CheckState.Unchecked:
+                    nextState = CheckState.Checked;
+                    break;
+                default:
+                    nextState = CheckState.Unchecked;
+                    break;
+            }
+
+            // notify the treeview that an update is about to take place
+            BeginUpdate();
+
+            // update the node state, and dependend nodes
+            node.SetCheckedState(nextState);
+
+            // force a redraw
+            EndUpdate();
+        }
+    }
+}
