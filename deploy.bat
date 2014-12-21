a8ad8c79266c33b08f7a04470f2088cc866895fa
TGOSeraph <seraph.it@gmail.com>
2014-12-21 12:20:04 +0000

    Fixed build configuration to build x86 and x64 binaries

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-xcopy "%1StUtil.Native.Injection.Helper\bin\x64" "C:\Projects\StUtil.Core\StUtil.Dev.WinForm\bin\Debug\x64" /F /R /Y /I
-xcopy "%1StUtil.Native.Injection.Helper\bin\x86" "C:\Projects\StUtil.Core\StUtil.Dev.WinForm\bin\Debug\x86" /F /R /Y /I
-
-xcopy "%1StUtil.Native.Injection\bin\x64" "C:\Projects\StUtil.Core\StUtil.Dev.WinForm\bin\Debug\x64" /F /R /Y /I
-xcopy "%1StUtil.Native.Injection\bin\x86" "C:\Projects\StUtil.Core\StUtil.Dev.WinForm\bin\Debug\x86" /F /R /Y /I
