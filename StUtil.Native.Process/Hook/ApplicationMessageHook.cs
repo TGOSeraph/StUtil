03abe81ae0b03dc45efa773890dc8ffd9ad81686
TGOSeraph <simon.snoopy@gmail.com>
2014-12-28 10:25:47 +0000

    Updated to inherit from base hook

--- StUtil.Native.Process/Hook/ApplicationMessageHook.cs
+++ StUtil.Native.Process/Hook/ApplicationMessageHook.cs
@@ -12,56 +12,37 @@ using System.Windows.Forms;
 
 namespace StUtil.Native.Hook
 {
-    public abstract class ApplicationMessageHook
+    public abstract class ApplicationMessageHook : ApplicationHook
     {
-        public System.Diagnostics.Process Process { get; private set; }
-
         public ApplicationMessageHook(System.Diagnostics.Process targetProcess)
+            : base(targetProcess)
         {
-            this.Process = targetProcess;
         }
 
         protected abstract void MessageReceived(object sender, Generic.EventArgs<Message> e);
 
-        public void Hook()
+        protected override void ApplyHook(string args)
         {
-            if (this.Process.Id == System.Diagnostics.Process.GetCurrentProcess().Id)
+            MessageHook hook = null;
+            Subclasser subclass = null;
+            /* We will have been injected into the remote process using CreateRemoteThread meaning we will
+             * not be on the UI thread. Therefore we need to subclass the main window of the process and
+             * then trigger our message hook after the first message has been received as we will be on
+             * the UI thread.
+             */
+            subclass = new Subclasser(System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle, delegate(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam)
             {
-                MessageHook hook = null;
-                Subclasser subclass = null;
-                subclass = new Subclasser(System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle, delegate(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam)
+                //Unsubclass the window as we are performing a hook
+                subclass.Restore();
+                if (hook == null)
                 {
-                    subclass.Restore();
-                    if (hook == null)
-                    {
-                        hook = new MessageHook(new LocalHook());
-                        hook.SetHook();
-                        hook.MessageReceived += MessageReceived;
-                    }
-                    return false;
-                });
-                subclass.Hook();
-            }
-            else
-            {
-                RemoteProcess p = new RemoteProcess(this.Process);
-                p.Open();
-                p.LoadDotNetModule(Assembly.GetExecutingAssembly().Location, typeof(ApplicationMessageHook).FullName, "Implant", this.GetType().Assembly.Location + ";" + this.GetType().FullName);
-            }
-        }
-
-        public static int Implant(string args)
-        {
-            string[] split = args.Split(new char[] { ';' }, 2);
-            Assembly asm = Assembly.LoadFrom(split[0]);
-
-            Type t = asm.GetType(split[1]);
-
-            ApplicationMessageHook hook = (ApplicationMessageHook)Activator.CreateInstance(t, System.Diagnostics.Process.GetCurrentProcess());
-
-            hook.Hook();
-
-            return 1;
+                    hook = new MessageHook(new LocalHook());
+                    hook.SetHook();
+                    hook.MessageReceived += MessageReceived;
+                }
+                return false;
+            });
+            subclass.Hook();
         }
     }
 }
