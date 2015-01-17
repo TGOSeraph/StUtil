bb956ba54bf128b2384fde7dd167184134496d27
TGOSeraph <seraph.it@gmail.com>
2015-01-17 17:42:53 +0000

    Added to make performing searches easier

---
+++ StUtil.Core/Utilities/SearchHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StUtil.Utilities
+{
+    public class SearchHelper<TInput, TOutput>
+    {
+        private object SyncLock = new object();
+        private int activeThreadId = 0;
+
+        public void Search(TInput input, Func<TInput, TOutput> search, Action<TOutput, Exception> callback)
+        {
+            lock (SyncLock)
+            {
+                int id = ++this.activeThreadId;
+
+                Thread t = new Thread(delegate()
+                {
+                    try
+                    {
+                        lock (SyncLock)
+                        {
+                            TOutput results = search(input);
+                            if (id == this.activeThreadId)
+                            {
+                                activeThreadId = 0;
+                                callback(results, null);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        callback(default(TOutput), ex);
+                    }
+                });
+                t.Start();
+            }
+        }
+    }
+}
