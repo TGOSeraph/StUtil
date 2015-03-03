176492ae417a62429c08f99b436ed7bb496b6dc0
TGOSeraph <seraph.it@gmail.com>
2015-03-03 13:51:26 +0000

    Starting custom WinForms theme form

---
+++ StUtil.UI/Components/HostedComponent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StUtil.UI.Components
+{
+    public abstract class HostedComponent : Component
+    {
+        private ContainerControl parentControl;
+
+        [DefaultValue(null), Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public ContainerControl ContainerControl
+        {
+            [UIPermission(SecurityAction.LinkDemand, Window = UIPermissionWindow.AllWindows)]
+            [UIPermission(SecurityAction.InheritanceDemand, Window = UIPermissionWindow.AllWindows)]
+            get
+            {
+                return parentControl;
+            }
+            set
+            {
+                if (parentControl != value)
+                {
+                    parentControl = value;
+                    OnInitialise();
+                }
+            }
+        }
+
+        public override ISite Site
+        {
+            set
+            {
+                base.Site = value;
+                if (value == null)
+                    return;
+
+                IDesignerHost host = value.GetService(typeof(IDesignerHost)) as IDesignerHost;
+                if (host != null)
+                {
+                    IComponent baseComp = host.RootComponent;
+
+                    if (baseComp is ContainerControl)
+                    {
+                        this.ContainerControl = (ContainerControl)baseComp;
+                    }
+                }
+            }
+        }
+
+        public HostedComponent()
+        {
+        }
+
+        public HostedComponent(ContainerControl parentControl)
+            : this()
+        {
+            this.parentControl = parentControl;
+        }
+
+        public HostedComponent(IContainer container)
+            : this()
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            container.Add(this);
+        }
+
+        protected virtual void OnInitialise()
+        {
+            if (ContainerControl.IsHandleCreated)
+            {
+                OnControlCreated();
+            }
+            else
+            {
+                ContainerControl.HandleCreated += ContainerControl_HandleCreated;
+            }
+        }
+
+        void ContainerControl_HandleCreated(object sender, EventArgs e)
+        {
+            OnControlCreated();
+        }
+
+        protected virtual void OnControlCreated()
+        {
+        }
+    }
+}
---
+++ StUtil.UI/Forms/Theme/CustomForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StUtil.UI.Forms.Theme
+{
+    public partial class CustomForm : Form
+    {
+        public Size? CustomSize { get; set; }
+
+        public CustomForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void SetBoundsCore(int x, int y, int width, int height, BoundsSpecified specified)
+        {
+            if (CustomSize != null)
+            {
+                width = CustomSize.Value.Width;
+                height = CustomSize.Value.Height;
+                CustomSize = null;
+            }
+            base.SetBoundsCore(x, y, width, height, specified);
+        }
+    }
+}
