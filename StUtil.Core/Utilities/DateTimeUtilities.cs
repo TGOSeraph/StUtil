aaa919a36e3945244619c22bd5e1ed7e77cbf669
TGOSeraph <seraph.it@gmail.com>
2015-03-15 11:35:49 +0000

    Helpful Date functions

---
+++ StUtil.Core/Utilities/DateTimeUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StUtil.Utilities
+{
+    public static class DateTimeUtilities
+    {
+        public static DateTime FromUnixTimestamp(int unixTimeStamp)
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+    }
+}
