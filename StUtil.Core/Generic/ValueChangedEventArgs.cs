df9b653246e627dec2baaca361f5a42b844030b4
TGOSeraph <seraph.it@gmail.com>
2015-03-15 11:33:24 +0000

    Added generic class for events that represent changed values

---
+++ StUtil.Core/Generic/ValueChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StUtil.Generic
+{
+    public class ValueChangedEventArgs<T> : EventArgs
+    {
+        public T NewValue { get; private set; }
+        public T OldValue { get; private set; }
+
+        public ValueChangedEventArgs(T newValue, T oldValue)
+        {
+            this.NewValue = newValue;
+            this.OldValue = oldValue;
+        }
+    }
+}
